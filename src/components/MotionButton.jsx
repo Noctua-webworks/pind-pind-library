a966638b4bb040cae514070d6379a03c20f5cd6d
lovepreet <lvprit3119@gmail.com>
2025-04-02 23:06:01 +0530

    new updates

---
+++ src/components/MotionButton.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+import React from 'react'
+import { motion } from "framer-motion";
+
+
+function MotionButton({title}) {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push("/contact");
+    };
+    return (
+        <motion.button
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.3 }}
+            className="cursor-pointer mt-6 px-6 py-3 bg-white text-[#58130a] font-semibold rounded-lg shadow-lg"
+            onClick={handleClick}
+        >
+            {title}
+        </motion.button>
+    )
+}
+
+export default MotionButton
